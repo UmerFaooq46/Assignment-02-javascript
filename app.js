6e51a47e3ba26eb076d4e453dc8a33db70127ea3
UmarFarooq <153651315+UmerFaooq46@users.noreply.github.com>
2024-03-06 17:17:18 +0500

    javascript first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+//   kamil case
+var myVariable;
+var _underscoreVariable;
+var camelCaseVariable;
+var $dollarSignVariable;
+var variable123;
+
+    //    multipication tables
+var userNumber = prompt("Enter a number for multiplication table:") || 6 ;
+
+
+for (var i = 1; i <= 10; i++) {
+    document.write(userNumber + " * " + i + " = " + userNumber * i + "<br>");
+}
+
